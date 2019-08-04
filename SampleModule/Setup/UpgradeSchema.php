44b629ca397cb40bbe5ea0baa856952483d4a86d
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-04 11:41:35 -0700

    UpgradeData and UpgradeSchema

---
+++ SampleModule/Setup/UpgradeSchema.php
@@ -0,0 +1,29 @@
+<?php
+    namespace Mastering\SampleModule\Setup;
+    use Magento\Framework\Setup\UpgradeSchemaInterface;
+    use Magento\Framework\Setup\ModuleContextInterface;
+    use Magento\Framework\Setup\SchemaSetupInterface;
+    use Magento\Framework\DB\Ddl\Table;
+
+    class UpgradeSchema implements UpgradeSchemaInterface
+    {
+        public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+        {
+            $setup->startSetup();
+
+            if( version_compare( $context->getVersion(), '1.0.1', '<' ) ) {
+                $setup->getConnection()->addColumn(
+                    $setup->getTable('mastering_sample_item'),
+                    'description',
+                    [
+                        'type' => Table::TYPE_TEXT,
+                        'nullable' => true,
+                        'comment' => 'Item Description'
+                    ]
+                );
+            }
+
+            
+            $setup->endSetup();
+        }
+    }
